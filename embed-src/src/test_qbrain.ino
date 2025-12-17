23c32331096a65cc988721a843ec924469db197f
arugoa <arihantjaingoa@gmail.com>
2025-12-17 06:59:25 -0800

    tester code for the motors

---
+++ embed-src/src/test_qbrain.ino
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <Servo.h>
+
+Servo escFL;
+Servo escFR;
+Servo escRR;
+Servo escRL;
+
+#define ESC_MIN 1000
+#define ESC_MAX 2000
+
+void setup() {
+  escFL.attach(3);
+  escFR.attach(5);
+  escRR.attach(6);
+  escRL.attach(9);
+
+  // ARM ESCs
+  escFL.writeMicroseconds(ESC_MIN);
+  escFR.writeMicroseconds(ESC_MIN);
+  escRR.writeMicroseconds(ESC_MIN);
+  escRL.writeMicroseconds(ESC_MIN);
+
+  delay(5000);
+}
+
+void loop() {
+  int throttle = 1200;
+
+  escFL.writeMicroseconds(throttle);
+  escFR.writeMicroseconds(throttle);
+  escRR.writeMicroseconds(throttle);
+  escRL.writeMicroseconds(throttle);
+
+  delay(20); // 50Hz
+}
