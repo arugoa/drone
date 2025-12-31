60496928ea51e16c23d803c1c736a9d91751f282
mtuteja1 <manan.tuteja5@gmail.com>
2025-12-31 01:26:07 -0800

    Flight time and power draw

--- AeroCalculations/DroneSetup.py
+++ AeroCalculations/DroneSetup.py
@@ -1,4 +1,3 @@
-
 import math
 # input parameters
 
@@ -11,12 +10,14 @@ MotorAssemblyMass = 52
 ArduinoMass = 18
 BatteryMass = 140
 
-
 # Battery
+BatteryNum = 1
 MaxCellV = 4.2
 Cells = 3
 # TotalVoltage = MaxCellV * Cells # Calculate the total voltage for the battery pack
 TotalVoltage = 11.1 # Conservative Estimate
+BatteryCapacity = 2700 # in mAh
+BatteryCapacity = BatteryCapacity/1000
 
 # Motor Stats
 MotorKV = 1100
@@ -25,18 +26,32 @@ PeakCurrent = 9.9 # Amps
 
 # Calculation
 # Calculate the maximum RPM of the motor
-MaxNoLoadRPM = MotorKV * TotalVoltage
-MotorVelocityConstant = MotorKV * 2 * math.pi / 60
-MaxLoadRPM = MaxNoLoadRPM - PeakCurrent * InternalResistance / MotorVelocityConstant
+MaxLoadRPM = MotorKV * (TotalVoltage- (PeakCurrent * InternalResistance))
 MaxLoadRevsPerS = MaxLoadRPM / 60
 
+
 # Thrust Calculation
-CT = 0.08 # Thrust coefficient [0.08,0.12] (rough guesstimate trust aero major)
+CT = 0.11 # Thrust coefficient [0.08,0.12] (rough guesstimate trust aero major)
 rho = 1.225 # Density [kg/m^3]
 D = 9 * 2.54 / 100 # Propeller Diameter in meters
 
 MaxThrustPerMotor = CT * rho * MaxLoadRevsPerS**2 * D ** 4
 MaxTotalThrust = MaxThrustPerMotor * 4
 NomninalThrust = MaxTotalThrust/TWR
-MaxWeight = NomninalThrust/9.81
-print(f'Theoretical Max Weight:{MaxWeight:6.2f} kg')
+MaxMass = NomninalThrust/9.81
+print(f'Theoretical Max Mass:{MaxMass:6.2f} kg')
+CurrentMass = (ChassisMass + (ESCMass + MotorAssemblyMass) * 4 + ArduinoMass + BatteryMass * BatteryNum )/1e3
+MassMargin = MaxMass - CurrentMass
+print("Current Mass:", CurrentMass, "kg")
+print("Mass Margin:", MassMargin, "kg")
+
+# Flight Time and Power Calculations
+CurrentWeight = CurrentMass * 9.81
+ThurstHover = CurrentWeight
+nHover = math.sqrt(ThurstHover/(CT * rho * D**4))
+
+CP = 0.06 #see UIUC database, just a roughguesstimate
+P = CP * rho * nHover ** 3 * D ** 5 #idealized power draw
+AmpDraw = P/TotalVoltage + 1 # Plus one amp to account for other electronics
+tflight = BatteryCapacity * 0.8 * 60 / AmpDraw
+print("Theoretical max flight time:", tflight, "min")
